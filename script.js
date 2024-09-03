55a087f07b8f3f6b72db1babc5d304122b8806cb
NoobAndCoding <just.gaming554t644@gmail.com>
2024-09-03 12:24:33 +0300

    added things and fixes

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("sideMenu").style.width = "250px";
+    document.getElementById("contentArea").style.marginLeft = "250px"
+}
+
+function closeNav() {
+    document.getElementById("sideMenu").style.width = "0px";
+    document.getElementById("contentArea").style.marginLeft = "0px"
+}
+
